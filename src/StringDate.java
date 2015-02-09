001f9bd42c120ef2cbb4069fb91e26975818b781
ago26 <ago26@email.vccs.edu>
2015-02-09 13:11:15 -0500

    ASSIGNMENT1

--- src/StringDate.java
+++ src/StringDate.java
@@ -1,8 +1,23 @@
+		import java.text.SimpleDateFormat;
+		import java.util.Date;
 
+		import javax.swing.JOptionPane;
+		
 public class StringDate {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+				Date d = new Date();
+				SimpleDateFormat sdf = new SimpleDateFormat ("dd.mm.yyyy");
+				String s = sdf.format(d);
+				System.out.println(s);
+				JOptionPane.showMessageDialog(null, (s));
+
+
+			
+			
+			
+
 
 	}
 
